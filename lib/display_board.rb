66d20446b18f7e34074e98e2ea4ea3ac2a8d356d
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-20 21:59:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board(board = "[" "," "," "," "," "," "," "," "," "]")
 
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
-  puts "-----------"# Define display_board that accepts a board and prints
-  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts "   |   |   "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end# out the current state.
